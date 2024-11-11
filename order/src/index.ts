4bb2665445a2ee004508e7d54e36dc57efa03659
Matias Sfer <matias0dveloper@gmail.com>
2024-11-11 15:57:11 -0300

    Implement supabase

--- order/src/index.ts
+++ order/src/index.ts
@@ -1,65 +1,78 @@
 import express from 'express';
 import https from 'https';
 import cors from 'cors';
-import { readFileSync } from 'fs';
 import { WebSocketServer } from 'ws';
-
 import { AppDataSource } from './config/data-source';
 import OrderRouter from './routes/order.routes';
 import { requestLogger } from './middleware/logger';
 import { globalSubscribers } from './messages/order.messages';
 
+const fetchSslFiles = async () => {
+    const privateKeyResponse = await fetch('https://wprvfcfwmkpjvvatrsbm.supabase.co/storage/v1/object/public/ssl/order/service-private-key.pem');
+    const certificateResponse = await fetch('https://wprvfcfwmkpjvvatrsbm.supabase.co/storage/v1/object/public/ssl/order/service-certificate.pem');
+
+    if (!privateKeyResponse.ok || !certificateResponse.ok) {
+        throw new Error('Failed to fetch SSL certificates');
+    }
+
+    const privateKey = await privateKeyResponse.text();
+    const certificate = await certificateResponse.text();
+
+    return { key: privateKey, cert: certificate };
+};
+
 const app = express();
 const PORT = process.env.PORT || 3003;
 
-const sslOptions = {
-    key: readFileSync('../service-certificate.pem'),
-    cert: readFileSync('../service-private-key.pem'),
-};
+const setupServer = async () => {
+    try {
+        const sslOptions = await fetchSslFiles();
 
-const server = https.createServer(sslOptions, app);
+        const server = https.createServer(sslOptions, app);
 
-const corsOptions = {
-    origin: "*",
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-};
+        const corsOptions = {
+            origin: "*",
+            methods: ['GET', 'POST', 'PUT', 'DELETE'],
+            allowedHeaders: ['Content-Type', 'Authorization'],
+        };
 
-app.use(express.json());
-app.use(requestLogger);
-app.use(cors(corsOptions));
+        app.use(express.json());
+        app.use(requestLogger);
+        app.use(cors(corsOptions));
 
-app.use('/order', OrderRouter);
+        app.use('/order', OrderRouter);
 
-server.listen(PORT);
+        server.listen(PORT, () => {
+            console.log(`Order service running on https://localhost:${PORT}`);
+        });
 
-AppDataSource.initialize()
-    .then(() => {
-        console.log(`Order service running on http://localhost:${PORT}`);
-    })
-    .catch((error) => {
-        console.error('Error during Data Source initialization:', error);
-    });
+        await AppDataSource.initialize();
+        console.log("Data Source has been initialized!");
 
-export const wss = new WebSocketServer({ server });
+        const wss = new WebSocketServer({ server });
 
-wss.on('connection', (ws) => {
-    console.log('Client connected');
-    globalSubscribers.add(ws);
-    ws.on('message', (message: string) => {
-        try {
-            const { orderId } = JSON.parse(message);
-            console.log(`Message received from client: ${JSON.stringify({ orderId })}`);
+        wss.on('connection', (ws) => {
+            console.log('Client connected');
             globalSubscribers.add(ws);
-            console.log('Client subscribed to all orders');
-        } catch (error) {
-            console.error('Error handling WebSocket message:', error);
-        }
-    });
+            ws.on('message', (message: string) => {
+                try {
+                    const { orderId } = JSON.parse(message);
+                    console.log(`Message received from client: ${JSON.stringify({ orderId })}`);
+                    globalSubscribers.add(ws);
+                    console.log('Client subscribed to all orders');
+                } catch (error) {
+                    console.error('Error handling WebSocket message:', error);
+                }
+            });
 
-    ws.on('close', () => {
-        globalSubscribers.delete(ws);
-        console.log('Client disconnected');
-    });
-});
+            ws.on('close', () => {
+                globalSubscribers.delete(ws);
+                console.log('Client disconnected');
+            });
+        });
+    } catch (error) {
+        console.error('Error setting up the server:', error);
+    }
+};
 
+setupServer();
--- warehouse/src/index.ts
+++ warehouse/src/index.ts
@@ -8,59 +8,83 @@ import stockRouter from './routes/stock.routes';
 import { AppDataSource } from './config/data-source';
 import { requestLogger } from './middleware/logger';
 import { globalSubscribers } from './messages/stock.messages';
-import { readFileSync } from 'fs';
-
 
 const app = express();
 
-const sslOptions = {
-    key: readFileSync('./private-key.pem'),
-    cert: readFileSync('./certificate.pem'),
+const sslFiles = async () => {
+    const privateKeyResponse = await fetch('https://wprvfcfwmkpjvvatrsbm.supabase.co/storage/v1/object/public/ssl/warehouse/private-key.pem');
+    const certificateResponse = await fetch('https://wprvfcfwmkpjvvatrsbm.supabase.co/storage/v1/object/public/ssl/warehouse/certificate.pem');
+
+    // Ensure both responses are successful
+    if (!privateKeyResponse.ok || !certificateResponse.ok) {
+        throw new Error('Failed to fetch SSL certificates');
+    }
+
+    // Get the text content of the SSL files
+    const privateKey = await privateKeyResponse.text();
+    const certificate = await certificateResponse.text();
+
+    return { key: privateKey, cert: certificate };
 };
 
-const server = https.createServer(sslOptions, app);
+const setupServer = async () => {
+    try {
+        // Get SSL certificates
+        const sslOptions = await sslFiles();
 
-const corsOptions = {
-    origin: "*",
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-};
+        // Create HTTPS server with the SSL certificates
+        const server = https.createServer(sslOptions, app);
 
-app.use(express.json());
-app.use(requestLogger);
-app.use(cors(corsOptions));
+        // CORS configuration
+        const corsOptions = {
+            origin: "*",
+            methods: ['GET', 'POST', 'PUT', 'DELETE'],
+            allowedHeaders: ['Content-Type', 'Authorization'],
+        };
 
-app.use('/stock', stockRouter);
-app.use('/purchase', purchaseRouter);
+        // Apply middlewares
+        app.use(express.json());
+        app.use(requestLogger);
+        app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 3002;
+        app.use('/stock', stockRouter);
+        app.use('/purchase', purchaseRouter);
 
-server.listen(PORT);
+        const PORT = process.env.PORT || 3002;
 
-AppDataSource.initialize()
-    .then(() => {
+        server.listen(PORT, () => {
+            console.log(`Server running on https://localhost:${PORT}`);
+        });
+
+        // Initialize Data Source (DB)
+        await AppDataSource.initialize();
         console.log("Data Source has been initialized!");
-    })
-    .catch((error) => {
-        console.error("Error during Data Source initialization", error);
-    });
 
-export const wss = new WebSocketServer({ server });
+        // WebSocket server setup
+        const wss = new WebSocketServer({ server });
 
-wss.on('connection', (ws) => {
-    console.log('Client connected');
-    globalSubscribers.add(ws);
-    ws.on('message', (_message: string) => {
-        try {
+        wss.on('connection', (ws) => {
+            console.log('Client connected');
             globalSubscribers.add(ws);
-            console.log('Client subscribed to all stock changes');
-        } catch (error) {
-            console.error('Error handling WebSocket message:', error);
-        }
-    });
+            ws.on('message', (_message: string) => {
+                try {
+                    globalSubscribers.add(ws);
+                    console.log('Client subscribed to all stock changes');
+                } catch (error) {
+                    console.error('Error handling WebSocket message:', error);
+                }
+            });
 
-    ws.on('close', () => {
-        globalSubscribers.delete(ws);
-        console.log('Client disconnected');
-    });
-});
+            ws.on('close', () => {
+                globalSubscribers.delete(ws);
+                console.log('Client disconnected');
+            });
+        });
+
+    } catch (error) {
+        console.error('Error setting up the server:', error);
+    }
+};
+
+// Start the server
+setupServer();
